ae8d5414744f4f4d56b85ec51e2a70abfe4b1f91
Brad Davis <brad@transparentproperties.com>
2017-10-07 21:22:48 -0700

    simple script working

--- root/unionfs-simple-cleanup.sh
+++ root/unionfs-simple-cleanup.sh
@@ -2,13 +2,25 @@
 
 UNIONFSRWPATH=/local
 PLEXDRIVEPATH=/plexdrive
+METADATAPATH=/.unionfs-fuse
 
-find $UNIONFSRWPATH/.unionfs-fuse -name '*_HIDDEN~' | while read line; do
-    oldPath=${line#/$UNIONFSRWPATH/.unionfs-fuse}
-    newPath=$PLEXDRIVEPATH${oldPath%_HIDDEN~}
-    echo "$newPath"
-    echo "$line"
+find $UNIONFSRWPATH$METADATAPATH -name '*_HIDDEN~' | while read line; do
+
+    FULLMETADATAPATH=$UNIONFSRWPATH$METADATAPATH${line#$UNIONFSRWPATH$METADATAPATH}
+    echo Metadata Path:   $FULLMETADATAPATH
+
+
+    CLOUDPATH="${FULLMETADATAPATH/$UNIONFSRWPATH$METADATAPATH/$PLEXDRIVEPATH}"
+    CLOUDPATH=${CLOUDPATH%_HIDDEN~}
+    echo Cloud Path:   $CLOUDPATH
+
+
+    rm "$FULLMETADATAPATH"
+    rm "$CLOUDPATH"
+
+    echo "=========================================="
+
 done
 
-find "$UNIONFSRWPATH/.unionfs-fuse" -mindepth 1 -type d -empty -delete
+find $UNIONFSRWPATH$METADATAPATH -depth -type d -exec rmdir {} \;
 exit
